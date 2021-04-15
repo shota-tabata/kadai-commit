f86f885c4e51f5a5563fb1c66a47e5016255820f
shota.tabata <yotubato4289@gmail.com>
2021-04-15 13:58:08 +0000

    sample.php

--- sample.php
+++ sample.php
@@ -19,7 +19,7 @@
     <body>
         <h1>おみくじ</h1>
         <p>おみくじの結果：<?php print htmlspecialchars($result, ENT_QUOTES, "UTF-8"); ?></p>
-        <form action="sample.php method="POST">
+        <form action="sample.php" method="POST">
             <button type="submit" name="act" value="draw">おみくじをひく！</button>
         </form>
     </body>
